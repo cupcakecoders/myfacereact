771a2bc4ce9a53aa9e021fb0097fd47a8bc618d9
Kanwaljit Dilawari <Kanwaljit.Dilawari@softwire.com>
2020-02-27 14:24:01 +0000

    add more css sytles to posts page

--- src/Post/Post.js
+++ src/Post/Post.js
@@ -5,12 +5,14 @@ import './Post.scss';
 export function Post(props) {
     console.log(props)
     return (
-    <ul>
+    <div className="post-container"post-style="true">
+    <ul post-style="true">
         <li post-style="true" className="post">
             <img post-style="true" className="post-image" alt="users favorite scenes" src={props.data.imageUrl}/>
             <h3 post-style="true">{props.data.postedBy.displayName}</h3>
             <p post-style="true">{props.data.message}</p>
         </li>
     </ul>
+    </div>
     );
 };
