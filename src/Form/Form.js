163bcd18202ef9e2846d2d0f8595edf1fed1a37f
Kanwaljit Dilawari <Kanwaljit.Dilawari@softwire.com>
2020-02-28 11:21:54 +0000

    new user sign up form submits data to api

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -2,18 +2,22 @@ import React, {useState} from "react";
 
 export function SubscribeForm() {
     const [email, setEmail] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [username, setUsername] = useState("");
 
-    function updateEmail(event){
-        setEmail(event.target.value);
-    }
+
+    // function createNewUser(event){
+    //     setEmail(event.target.value);
+    //}
 
     function handleSubmit(event) {
         event.preventDefault();
         const signupEmail = {
             email: email,
-            firstName: "mike",
-            lastName: "walker",
-            username: "mike1"
+            firstName: firstName,
+            lastName: lastName,
+            username: username
         };
         fetch("https://localhost:5001/users/create", {
             method: "POST",
@@ -30,7 +34,19 @@ export function SubscribeForm() {
         <form onSubmit={handleSubmit}>
             <label>
                 Email: 
-                <input type="email" value={email} onChange={updateEmail}/>
+                <input type="email" value={email} onChange={event => setEmail(event.target.value)}/>
+            </label>
+            <label>
+                First Name: 
+                <input type="text" value={firstName} onChange={event => setFirstName(event.target.value)}/>
+            </label>
+            <label>
+                Last Name: 
+                <input type="text" value={lastName} onChange={event => setLastName(event.target.value)}/>
+            </label>
+            <label>
+                Username: 
+                <input type="text" value={username} onChange={event => setUsername(event.target.value)}/>
             </label>
             <input type="submit" value="Submit"/>
         </form>
